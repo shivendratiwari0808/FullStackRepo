77fcb6a391f94a755874a2473290494c087ae88f
omkar81-pa <omkarpathak606@gmail.com>
2025-01-09 18:00:35 +0530

    jhg

--- src/com/nit/test/A.java
+++ src/com/nit/test/A.java
@@ -3,12 +3,8 @@ package com.nit.test;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Omkar");
+		System.out.println("Omkar");
 		System.out.println("Omserkjfh");
-		System.out.println("wureihu");
-		System.out.println("wureihu");
-		System.out.println("wureihu");
-		System.out.println("wureihu");
-		System.out.println("wureihu");
-		System.out.println("wureihu");
+		
 	}
 }
