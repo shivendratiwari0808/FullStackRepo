e432e1d5a4a9bf3c916849f77d7608fc682c2020
omkar81-pa <omkarpathak606@gmail.com>
2025-01-09 18:04:45 +0530

    mkjh

--- src/com/nit/test/A.java
+++ src/com/nit/test/A.java
@@ -3,11 +3,7 @@ package com.nit.test;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Omkar");
-		System.out.println("Omkar");
-		System.out.println("Omserkjfh");
-		System.out.println("Omserkjfh");
-		System.out.println("Omserkjfh");
-		System.out.println("Omserkjfh");
+		System.out.println("Omkjhgjhjhnbvhgfhgbjguar");
 		
 	}
 }
